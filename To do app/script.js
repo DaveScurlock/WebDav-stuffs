0e78be50f28f507b4e6dff5fe4578dfb6c0b97ca
Dave Scurlock <d-richardson@hotmail.co.uk>
2022-04-21 16:43:52 +0100

    commit

--- To do app/script.js
+++ To do app/script.js
@@ -1,2 +1,17 @@
 
+let inputField = document.getElementById('title');
+let button = document.getElementById('addToDo')
+let list = document.getElementById('listContent')
 
+button.addEventListener('click', function() {
+
+    let paragraph = document.createElement('p');
+    paragraph.innerText = inputField.value;
+    list.appendChild(paragraph);
+    inputField.value = ""
+
+})
+
+
+          
+    
