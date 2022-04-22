33ddd217e2d35b26b351fcd69b7ce772b5a51e2a
Dave Scurlock <d-richardson@hotmail.co.uk>
2022-04-22 01:11:19 +0100

    c

--- To do app/script.js
+++ To do app/script.js
@@ -1,16 +1,57 @@
 
-let inputField = document.getElementById('title');
-let button = document.getElementById('addToDo')
-let list = document.getElementById('listContent')
+let inputTitle = document.getElementById('title');
+let inputDescription = document.getElementById('description');
 
-button.addEventListener('click', function() {
 
-    let paragraph = document.createElement('p');
-    paragraph.innerText = inputField.value;
-    list.appendChild(paragraph);
-    inputField.value = ""
+let button = document.getElementById('addToDo');
+let list = document.getElementById('listContentActual');
+
+//control empty space
+
+button.addEventListener('mousedown', function() {
+
+    if (inputTitle.value === "" || inputDescription.value === "") {
+        button.removeEventListener('mouseup', addEvent);
+        document.getElementById('note').style.display = 'block';
+    } else {
+        button.addEventListener('mouseup', addEvent)
+        document.getElementById('note').style.display = 'none';
+    }
+    })
+
+//Button color on press
+
+button.addEventListener('mousedown', function() {
+
+    button.style.backgroundColor = 'midnightblue';
+    button.style.color = 'lightgray';
 
 })
+
+button.addEventListener('mouseup', function() {
+
+    button.style.backgroundColor = '';
+    button.style.color = ''
+
+})
+
+// Add task to list
+
+function addEvent() {
+    let title = inputTitle.value;
+    let description = inputDescription.value;
+    
+   
+    let paragraph = document.createElement('li');
+    
+    
+    paragraph.innerText = `${title} - ${description}`;
+     
+    
+    listContentActual.appendChild(paragraph);
+    inputTitle.value = "";
+    inputDescription.value = "";
+}
 
 
           
